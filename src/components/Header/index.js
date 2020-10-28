7c8454ecbacb51012112ab3bc96eabea6eee931c
Yadira Tamarit <ystamaritq@gmail.com>
2020-10-28 18:25:12 -0500

    css btn header updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,9 @@ const Header = () => {
 	return (
 		<div className="hero-image">
 			<div className="hero-text">
-				<h1 style={{ fontSize: "70px" }}>Hi, I'm Adelfo</h1>
+				<h1 style={{ fontSize: "70px", marginBottom: "20px" }}>
+					Hi, I'm Adelfo
+				</h1>
 				<h2 style={{ marginBottom: "20px" }}>And I'm a Builder</h2>
 				<Button variant="contained" className="btn-style">
 					<span>Contact Me</span>
