e9cf8f1190dd496b0dbc1fdcd5629fa26edc5a8f
Yadira Tamarit <ystamaritq@gmail.com>
2020-10-28 13:08:42 -0500

    added header template text

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./style.css";
 
 const Header = () => {
-	return <div className="header">Header Section</div>;
+	return (
+		<div className="hero-image">
+			<div className="hero-text">
+				<h1 style={{ fontSize: "50px" }}>I am Adelfo</h1>
+				<h3>And I'm a Builder</h3>
+				<button>Contact me</button>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
