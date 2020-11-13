bb7427a3b7d3735f56b85ee003e31505ccbe48ff
Arnaldo Carreno <acarreno@us.ibm.com>
2020-11-13 08:59:34 -0600

    Adsd Privacy and Terms placeholders to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,29 @@
 import React from "react";
-import "./style.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  section: {
+    padding: theme.spacing(5),
+    textAlign: "center",
+    borderTop: "1px solid",
+    borderColor: theme.palette.primary.light,
+    color: theme.palette.primary.main,
+  },
+}));
 
 const Footer = () => {
-	return <div className="footer">Footer</div>;
+  const classes = useStyles();
+  const year = new Date().getFullYear();
+  return (
+    <div id="footer" className={classes.section}>
+      <p>
+        © {year} A.G. Residential Landscapes |
+        <Button color="primary">Privacy</Button> |{" "}
+        <Button color="primary">Terms</Button>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
