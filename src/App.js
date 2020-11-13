739775e99880fb1aca1ccfff8c5936006163c394
Arnaldo Carreno <acarreno@us.ibm.com>
2020-11-13 08:57:38 -0600

    Added site theme with palette

--- src/App.js
+++ src/App.js
@@ -5,18 +5,33 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Work from "./components/Work";
+import FabContact from "./components/FabContact";
 import "./App.css";
+import { createMuiTheme } from "@material-ui/core/styles";
+import teal from "@material-ui/core/colors/teal";
+import green from "@material-ui/core/colors/green";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: green,
+  },
+});
 
 const App = () => {
   return (
-    <div className="app-style">
-      <Navigation />
-      <Header />
-      <Services />
-      <Work />
-      <Contact />
-      <Footer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="app-style">
+        <Navigation />
+        <Header />
+        <Services />
+        <Work />
+        <Contact />
+        <Footer />
+      </div>
+      <FabContact />
+    </ThemeProvider>
   );
 };
 
